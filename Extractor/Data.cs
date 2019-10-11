76cbf7b2b14d2a60dff477051f1786cc25868f8c
james churchouse <james.churchouse@aiglife.co.uk>
2019-10-11 13:50:20 +0100

    Add project files.

---
+++ Extractor/Data.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extractor
+{
+    class Data
+    {
+        public decimal Date{get;set;}
+public decimal RegionName{get;set;}
+public decimal AreaCode{get;set;}
+public decimal AveragePrice{get;set;}
+public decimal Index{get;set;}
+public decimal IndexSA{get;set;}
+public decimal 1m%Change{get;set;}
+public decimal 12m%Change{get;set;}
+public decimal AveragePriceSA{get;set;}
+public decimal SalesVolume{get;set;}
+public decimal DetachedPrice{get;set;}
+public decimal DetachedIndex{get;set;}
+public decimal Detached1m%Change{get;set;}
+public decimal Detached12m%Change{get;set;}
+public decimal SemiDetachedPrice{get;set;}
+public decimal SemiDetachedIndex{get;set;}
+public decimal SemiDetached1m%Change{get;set;}
+public decimal SemiDetached12m%Change{get;set;}
+public decimal TerracedPrice{get;set;}
+public decimal TerracedIndex{get;set;}
+public decimal Terraced1m%Change{get;set;}
+public decimal Terraced12m%Change{get;set;}
+public decimal FlatPrice{get;set;}
+public decimal FlatIndex{get;set;}
+public decimal Flat1m%Change{get;set;}
+public decimal Flat12m%Change{get;set;}
+public decimal CashPrice{get;set;}
+public decimal CashIndex{get;set;}
+public decimal Cash1m%Change{get;set;}
+public decimal Cash12m%Change{get;set;}
+public decimal CashSalesVolume{get;set;}
+public decimal MortgagePrice{get;set;}
+public decimal MortgageIndex{get;set;}
+public decimal Mortgage1m%Change{get;set;}
+public decimal Mortgage12m%Change{get;set;}
+public decimal MortgageSalesVolume{get;set;}
+public decimal FTBPrice{get;set;}
+public decimal FTBIndex{get;set;}
+public decimal FTB1m%Change{get;set;}
+public decimal FTB12m%Change{get;set;}
+public decimal FOOPrice{get;set;}
+public decimal FOOIndex{get;set;}
+public decimal FOO1m%Change{get;set;}
+public decimal FOO12m%Change{get;set;}
+public decimal NewPrice{get;set;}
+public decimal NewIndex{get;set;}
+public decimal New1m%Change{get;set;}
+public decimal New12m%Change{get;set;}
+public decimal NewSalesVolume{get;set;}
+public decimal OldPrice{get;set;}
+public decimal OldIndex{get;set;}
+public decimal Old1m%Change{get;set;}
+public decimal Old12m%Change{get;set;}
+public decimal OldSalesVolume{get;set;}
+
+    }
+}
---
+++ PropertyPrices/Program.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+using System;
+
+namespace PropertyPrices
+{
+
+
+    class Program
+    {
+
+           public static Logger Logger = LogManager.GetLogger("log");
+
+        static void Main(string[] args)
+        {
+            new PricePredictionRanker().Predict();
+        }
+    }
+}
